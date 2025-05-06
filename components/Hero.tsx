50493fa655c124131099f17f723c91dba3048721
Lemohang57 <lemohangmakintane57@gmail.com>
2025-05-06 02:04:43 +0200

    fixed-no-anonymous-default-export

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,26 @@
 "use client"
+import Image from 'next/image'
 import { useState } from 'react'
 
-export default () => {
-
+const HeroSection = () => {
     const [state, setState] = useState(false)
 
-  // Replace javascript:void(0) path with your path
-  const navigation = [
-      { title: "Customers", path: "javascript:void(0)" },
-      { title: "Careers", path: "javascript:void(0)" },
-      { title: "Guides", path: "javascript:void(0)" },
-      { title: "Partners", path: "javascript:void(0)" },
-      { title: "Team", path: "javascript:void(0)" }
-  ]
-  
+    // Replace javascript:void(0) path with your path
+    const navigation = [
+        { title: "Customers", path: "javascript:void(0)" },
+        { title: "Careers", path: "javascript:void(0)" },
+        { title: "Guides", path: "javascript:void(0)" },
+        { title: "Partners", path: "javascript:void(0)" },
+        { title: "Team", path: "javascript:void(0)" }
+    ]
+    
     return (
         <>
             <header>
                 <nav className="relative items-center pt-5 px-4 mx-auto max-w-screen-xl sm:px-8 sm:flex sm:space-x-6">
                     <div className="flex justify-between">
                         <a href="javascript:void(0)">
-                            <img
+                            <Image
                                 src="https://www.floatui.com/logo.svg" 
                                 width={120} 
                                 height={50}
@@ -36,7 +36,6 @@ export default () => {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 ) : (
-
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                                     </svg>
@@ -61,14 +60,14 @@ export default () => {
                 <div className="space-y-4 flex-1 sm:text-center lg:text-left">
                     <h1 className="text-gray-800 font-bold text-4xl xl:text-5xl">
                         Optimize your website for
-                         <span className="text-indigo-600"> Search engine</span>
+                        <span className="text-indigo-600"> Search engine</span>
                     </h1>
                     <p className="text-gray-500 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum
                     </p>
                     <div>
                         <p className="text-gray-800 py-3">
-                            Subscribe to our newsletter and we'll save your time
+                            Subscribe to our newsletter and we&apos;ll save your time
                         </p>
                         <form className="items-center space-y-3 sm:justify-center sm:space-x-3 sm:space-y-0 sm:flex lg:justify-start">
                             <input
@@ -83,9 +82,19 @@ export default () => {
                     </div>
                 </div>
                 <div className="flex-1 text-center mt-4 lg:mt-0 lg:ml-3">
-                    <img src="https://i.postimg.cc/kgd4WhyS/container.png" className="w-full mx-auto sm:w-10/12  lg:w-full" />
+                    <Image
+                    width={300}
+                    height={300}
+                    alt=''
+                     src="https://i.postimg.cc/kgd4WhyS/container.png" 
+                     className="w-full mx-auto sm:w-10/12  lg:w-full" 
+                     />
                 </div>
             </section>
         </>
     )
 }
+
+HeroSection.displayName = "HeroSection"
+
+export default HeroSection
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['tailwindui.com','flowbite.s3.amazonaws.com','assets.aceternity.com',"utfs.io", 'images.unsplash.com', 'pbs.twimg.com'],
+    domains: [
+      'tailwindui.com',
+      'flowbite.s3.amazonaws.com',
+      'assets.aceternity.com',
+      'utfs.io',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'i.postimg.cc', 
+    ],
   },
 };
-
 
 export default nextConfig;
